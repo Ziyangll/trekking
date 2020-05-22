38e73efa7ffb8a6e7cb4b3e87e4948c13fd2ad1c
ziyang li <zl@auburn.edu>
2020-05-22 16:36:12 -0500

    trying to combine react to node

---
+++ backend/build/precache-manifest.06d3270c3f0e5c2381ccc176709b2b3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3125c2d0be7d3e574a2a4c80eaf3a5b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac35823be1e93d4912b9",
+    "url": "/static/css/main.ce12a795.chunk.css"
+  },
+  {
+    "revision": "05795247e6c8cda94e20",
+    "url": "/static/js/2.a24c36ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a24c36ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac35823be1e93d4912b9",
+    "url": "/static/js/main.2166eef7.chunk.js"
+  },
+  {
+    "revision": "59573e8e2cc2adb5fe8b",
+    "url": "/static/js/runtime-main.8a298c79.js"
+  }
+]);
