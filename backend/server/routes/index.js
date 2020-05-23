47c79d1829632660aa999e0f49ffef62bd53b4ae
ziyang li <zl@auburn.edu>
2020-05-22 21:25:38 -0500

    does not work in google app engine for some reason

--- backend/server/routes/index.js
+++ backend/server/routes/index.js
@@ -4,10 +4,10 @@ const apiRoute = require('./apis');
 const path = require('path');
 
 function init(server) {
-    server.get('*', function (req, res, next) {
-        console.log('Request was made to: ' +req.originalUrl);
-        return next();
-    });
+    // server.get('*', function (req, res, next) {
+    //     console.log('Request was made to: ' +req.originalUrl);
+    //     return next();
+    // });
     server.get('/', function(req, res) {
         res.sendFile(path.join(__dirname, '../build', 'index.html'));
     });
